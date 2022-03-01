a85fbc6bdef1a60dd815448d3b352c9bd4202301
cyn <cynthiacui98@gmail.com>
2022-02-28 18:07:39 -0800

    linked githubs in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,5 +2,10 @@ import React from "react";
 import "../styles/styles.css";
 
 export default function Footer() {
-  return <div className="footer">Made by Jonathan Su and Cynthia Cui</div>;
+  return (
+    <div className="footer">
+      Made by <a href="https://github.com/hjonsu">Jonathan Su</a> and{" "}
+      <a href="https://github.com/cyncui">Cynthia Cui</a>
+    </div>
+  );
 }
